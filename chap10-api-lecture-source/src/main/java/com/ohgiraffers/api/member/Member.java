62ed81da3a8b28886886b075a8fa129ed0d1672a
SSEONGMIN-GUEST\hoodeok <wdh970616@naver.com>
2024-05-30 00:40:53 +0900

    집에서 api 활용 예제 풀이

--- chap10-api-lecture-source/src/main/java/com/ohgiraffers/api/member/Member.java
+++ chap10-api-lecture-source/src/main/java/com/ohgiraffers/api/member/Member.java
@@ -1,6 +1,5 @@
 package com.ohgiraffers.api.member;
 
-import java.time.LocalDate;
 import java.util.Date;
 
 public class Member {
@@ -11,12 +10,52 @@ public class Member {
     int yyyymmdd;
     Date birth = new Date(yyyymmdd);
 
-    public Member(int memberNo, String memberName, int height, int weight, int yyyymmdd) {
-        this.memberNo = memberNo;
+    public Member(String[] member) {
+        this.memberNo = Integer.parseInt(member[0]);
+        this.memberName = member[1];
+        this.height = Integer.parseInt(member[2]);
+        this.weight = Integer.parseInt(member[3]);
+        this.yyyymmdd = Integer.parseInt(member[4]);
+    }
+
+    public int getMemberNo() {
+        return memberNo;
+    }
+
+    public void setMemberNo(String memberNo) {
+        this.memberNo = Integer.parseInt(memberNo);
+    }
+
+    public String getMemberName() {
+        return memberName;
+    }
+
+    public void setMemberName(String memberName) {
         this.memberName = memberName;
-        this.height = height;
-        this.weight = weight;
-        this.yyyymmdd = yyyymmdd;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(String height) {
+        this.height = Integer.parseInt(height);
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(String weight) {
+        this.weight = Integer.parseInt(weight);
+    }
+
+    public int getYyyymmdd() {
+        return yyyymmdd;
+    }
+
+    public void setYyyymmdd(String yyyymmdd) {
+        this.yyyymmdd = Integer.parseInt(yyyymmdd);
     }
 
     public String information() {
--- chap10-api-lecture-source/src/main/java/com/ohgiraffers/api/member/Run.java
+++ chap10-api-lecture-source/src/main/java/com/ohgiraffers/api/member/Run.java
@@ -2,9 +2,19 @@ package com.ohgiraffers.api.member;
 
 public class Run {
     public static void main(String[] args) {
-//        Member member = new Member();
 
         String memberStr = "1,김연아,165,47,19900905|2,양세형,167,60,19850818|3,김래원,182,80,19810319";
-        
+        String[] membersStr = memberStr.split("\\|");
+        String[] memberInfo1 = membersStr[0].split(",");
+        String[] memberInfo2 = membersStr[1].split(",");
+        String[] memberInfo3 = membersStr[2].split(",");
+
+        Member member1 = new Member(memberInfo1);
+        Member member2 = new Member(memberInfo2);
+        Member member3 = new Member(memberInfo3);
+
+        System.out.println(member1.information());
+        System.out.println(member2.information());
+        System.out.println(member3.information());
     }
 }
