c74f6476c4af7f68a6661543af7a6471554d2944
wdh970616 <wdh970616@naver.com>
2024-05-31 16:08:55 +0900

    오타 수정

--- chap13-exception-lecture-source/src/main/java/com/ohgiraffers/section02/exception/PriceNegativeException.java
+++ chap13-exception-lecture-source/src/main/java/com/ohgiraffers/section02/exception/PriceNegativeException.java
@@ -1,6 +1,6 @@
 package com.ohgiraffers.section02.exception;
 
-public class PriceNegativeException extends Exception {
+public class PriceNegativeException extends NegativeException {
     public PriceNegativeException(String message) {
         super(message);
     }
