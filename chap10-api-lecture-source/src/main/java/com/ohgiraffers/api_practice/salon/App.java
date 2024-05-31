825483f51e78fc92971a52a0cf5f0d6719a08bed
wdh970616 <wdh970616@naver.com>
2024-05-31 17:46:59 +0900

    미용실 데이터 처리 시도

--- chap10-api-lecture-source/src/main/java/com/ohgiraffers/api_practice/salon/App.java
+++ chap10-api-lecture-source/src/main/java/com/ohgiraffers/api_practice/salon/App.java
@@ -5,22 +5,26 @@ import java.util.*;
 public class App {
     public static void main(String[] args) {
 
+        // 줄바꿈 단위로 나눠줌
         String[] firstSplitData = Data.data.split("\n");
+
+        // 첫째줄을 헤더리스트(키)로 초기화
+        List<String> headerList = new ArrayList<>(Arrays.asList(firstSplitData[0]));
+
+        // 줄바꿈 단위로 나눈 두번째 줄부터 infoData라는 스트링배열로 저장
         String[] infoData = new String[firstSplitData.length - 1];
         for (int i = 1; i < infoData.length; i++) {
             infoData[i - 1] = firstSplitData[i];
         }
 
-        List<String> headerList = new ArrayList<>(Arrays.asList(firstSplitData[0]));
+        String[] firstSlpitInfoData = Arrays.toString(infoData).split("\n");
+        String[] secondSplitInfoData = (Arrays.toString(firstSlpitInfoData)).split(",");
+        List<String> infoList = new ArrayList<>(Arrays.asList(secondSplitInfoData));
+
         List<Map<String, Object>> mapList = new ArrayList<>();
 
         int count = 0;
-        while (count < firstSplitData.length) {
-            String[] secondSplitData = new String[firstSplitData.length];
-            for (int j = 0; j < infoData.length; j++) {
-                secondSplitData = firstSplitData[j].split(",");
-            }
-            List<String> infoList = new ArrayList<>(Arrays.asList(secondSplitData));
+        while (count < firstSlpitInfoData.length) {
             Map<String, Object> map = new HashMap<>();
             for (int i = 0; i < headerList.size(); i++) {
                 map.put(headerList.get(i), infoList.get(i));
