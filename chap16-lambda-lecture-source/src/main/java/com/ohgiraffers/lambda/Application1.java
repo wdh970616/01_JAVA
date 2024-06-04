63310aeca0ae063d1f0c8b16040ef46200a51cc3
wdh970616 <wdh970616@naver.com>
2024-06-04 13:02:06 +0900

    lambda 마무리

--- chap16-lambda-lecture-source/src/main/java/com/ohgiraffers/lambda/Application1.java
+++ chap16-lambda-lecture-source/src/main/java/com/ohgiraffers/lambda/Application1.java
@@ -1,4 +1,66 @@
 package com.ohgiraffers.lambda;
 
 public class Application1 {
+    public static void main(String[] args) {
+        Application1 app = new Application1();
+        app.test1();
+        app.test2();
+        app.test3();
+        app.test4();
+        app.test5();
+    }
+
+    /*
+     * <pre>
+     * @실습문제1
+     * 현재시각 HH:mm:ss.SSS을 출력하는 람다식을 작성
+     * </pre>
+     */
+    private void test1() {
+
+    }
+
+    /*
+     * <pre>
+     * @실습문제2
+     * 로또를 생성하는 람다식을 작성하세요.
+     * - 리턴타입 Set<Integer>
+     * </pre>
+     */
+    private void test2() {
+
+    }
+
+    /*
+     * <pre>
+     * @실습문제3
+     * 원화 입력시 환율 계산을 통해 얻어진 달러를 출력하는 람다식을 작성
+     * 현재 1달러는 1350원이다.
+     * </pre>
+     */
+    private void test3() {
+
+    }
+
+    /*
+     * <pre>
+     * @실습문제4
+     * 위 문제를 jdk가 제공하는 Function 함수형인터페이스 형식에 맞게 변형하세요.
+     * </pre>
+     */
+    private void test4() {
+
+    }
+
+    /*
+     * <pre>
+     * @실습문제5
+     * 공백을 제외한 문자열의 길이가 0인지를 체크하는 람다식을 만들고,
+     * 문자열리스트 {"abc","","대한민국","   "}를 체크하세요.
+     * </pre>
+     */
+    private void test5() {
+//        List<String> strList = Arrays.asList("abc", "", "대한민국", "   ");
+
+    }
 }
